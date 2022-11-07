773cd107fe3e1c88daf05142a43b1c74e609757e
Jude Davis <judedavis92@gmail.com>
2022-11-07 10:45:56 +0000

    Refactoring createSession

--- src/backend/controllers/sessionController.ts
+++ src/backend/controllers/sessionController.ts
@@ -16,28 +16,29 @@ async function createSession(
   res: Response,
   next: Function
 ): Promise<void> {
-  // time: milliseconds since 1970
-  const { place, members, time } = JSON.parse(String(req.params));
+  try {
+    // time: milliseconds since 1970
+    const { place, members, time } = JSON.parse(String(req.params));
 
-  // Validate session attributes
-  const sessionInvalid = !place || !members || !time;
-  if (sessionInvalid)
-  {
-    res.status(400).send({error: "Invalid request!"});
+    // Validate session attributes
+    const sessionInvalid = !place || !members || !time;
+    if (sessionInvalid) {
+      res.status(400).json({ error: "Invalid request!" });
+      return;
+    }
+
+    Session.create({
+      place,
+      members,
+      time
+    }).then(() => {
+      res.status(200).json({ data: "Created session" });
+      next();
+    });
+  } catch (error) {
+    res.status(400).json({ error: "Couldn't create session!" });
     return;
   }
-
-  Session.create({
-    place,
-    members,
-    time
-  }).catch(() => {
-    res.status(400).send({error: "Couldn't create session!"});
-    return;
-  }).then(() => {
-    res.status(200).send({data: "Created session"});
-    next();
-  });
 }
 
 async function editSession(
