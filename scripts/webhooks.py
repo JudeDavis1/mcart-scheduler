f820a95f89cd1151bdbf8f7298623a6924a368c2
Jude Davis <judedavis92@gmail.com>
2022-12-04 22:35:44 +0000

    Update webhooks

--- scripts/webhooks.py
+++ scripts/webhooks.py
@@ -7,6 +7,7 @@ path = '/workspace/mcart-scheduler'
 @app.route('/webhooks/push', methods=['POST'])
 def on_push():
     print(request.get_json())
+    os.system('killall node')
     os.system(f'cd {path} && git checkout production && git pull && npm run build && sh startup.sh')
     return 'Done'
 
