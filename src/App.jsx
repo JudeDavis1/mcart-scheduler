fc58396e81761e147000863d02bedba44770a016
Jude Davis <judedavis92@gmail.com>
2022-12-04 22:40:11 +0000

    tests

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   }, []);
   return (
     <div className='App'>
-      <h1>hello</h1>
+      <h1>hel</h1>
     <NavigationMenu />
     <Routes>
       <Route element={ <Navigate to='/home' /> } path='/' />
