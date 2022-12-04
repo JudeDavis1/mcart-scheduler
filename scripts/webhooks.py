4f83e9a360c7e9f575fb6e944eecd5812702a4dd
Jude Davis <judedavis92@gmail.com>
2022-12-04 22:16:01 +0000

    Update webhooks.py

--- scripts/webhooks.py
+++ scripts/webhooks.py
@@ -2,11 +2,12 @@ import os
 from flask import Flask, request
 
 app = Flask(__name__)
-
+path = '/workspace/mcart-scheduler'
 
 @app.route('/webhooks/push', methods=['POST'])
 def on_push():
-    print(request)
+    os.system(f'cd {path} && git checkout production && git pull && npm run deploy')
+    return 'Done'
 
 
 app.run()
