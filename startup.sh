84978a2317dda7914aed4c79678137561312216c
Jude Davis <judedavis92@gmail.com>
2022-12-04 11:06:47 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This starts up the project (frontend and backend) in the background as services
-sudo nohup ./workspace/server.js &
+sudo nohup node ./workspace/server.js &
 sudo nohup serve -s build -l 80
