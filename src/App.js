4a2166d13a69d2a806ebce3174fbb715b31a1a82
Jude Davis <judedavis92@gmail.com>
2022-12-15 13:52:19 +0000

    Updates

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   }, []);
   return (
     <div className='App'>
-      <h1>helloss</h1>
+      <h1>hellsfdgoss</h1>
     <NavigationMenu />
     <Routes>
       <Route element={ <Navigate to='/home' /> } path='/' />
