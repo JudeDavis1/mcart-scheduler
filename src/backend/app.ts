ddcc689947b32097b30d8897899a628bffef1569
Jude Davis <judedavis92@gmail.com>
2022-12-20 16:02:49 +0000

    updates

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -22,8 +22,6 @@ app.use((req: Request, res: Response, next: Function) => {
         "Origin, X-Requested-With, Content-Type, Accept",
     );
 	res.header("Access-Control-Allow-Credentials", "true");
-
-    next();
 });
 app.use(cookieParser());
 app.use(express.json());
