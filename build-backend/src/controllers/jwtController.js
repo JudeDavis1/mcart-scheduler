9ffbc22fdd405ddce5f31922ad0c9e87cefa77c3
Jude Davis <judedavis92@gmail.com>
2022-12-20 19:37:02 +0000

    Added build

--- build-backend/src/controllers/jwtController.js
+++ build-backend/src/controllers/jwtController.js
@@ -2,21 +2,24 @@ import axios from 'axios';
 import config from '../config';
 function hasJwt(next) {
     let jwt;
-    axios.get(config.backend_url + '/user/verify', config.withCookies)
+    axios.get(config.backend_url + '/user/verify?' + document.cookie, config.withCookies)
         .then((val) => {
+        console.log('ISVALID');
+        console.log(val.data.isValid);
         if (val.data.isValid) {
             jwt = val.data.user;
             window.location.href = '/dashboard';
         }
     })
         .catch((err) => {
-        if (err)
+        if (err) {
             console.log(err);
+        }
     })
         .then(() => next ? next(jwt) : {});
 }
 async function getUser() {
-    const data = await axios.get(config.backend_url + '/user/verify', config.withCookies);
+    const data = await axios.get(config.backend_url + '/user/verify?' + document.cookie, config.withCookies);
     return data.data.user;
 }
 export { hasJwt, getUser };
